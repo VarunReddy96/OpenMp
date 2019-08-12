a6cfe13d432c25a2f572da86723af5925721f62b
nikhil keswaney <nikhilkeswaney1996@gmail.com>
2019-08-12 18:32:25 -0400

    New readmd file and commented code

--- PrimeNumber/PrimeSmp.java
+++ PrimeNumber/PrimeSmp.java
@@ -1,23 +1,38 @@
-
+/**
+ * Class PrimeSmp is a parallel program that tests numbers for primality. It
+ * prints the numbers on the command line that are prime.
+ **/
 class PrimeSeq {
-    private static boolean isPrime(long x)
-    {
+    /**
+     * Test the given number for primality.
+     *
+     * @param x Number &ge; 3.
+     * @return True if x is prime, false otherwise.
+     */
+    private static boolean isPrime(long x) {
         if (x % 2 == 0) return false;
         long p = 3;
-        long psqr = p*p;
-        while (psqr <= x)
-        {
+        long psqr = p * p;
+        while (psqr <= x) {
             if (x % p == 0) return false;
             p += 2;
-            psqr = p*p;
+            psqr = p * p;
         }
         return true;
     }
+
+    /**
+     * Main program.
+     */
     public static void main(String[] args) {
+
+        if (args.length < 1) {
+            System.out.println("Please enter atleast one number to check");
+        }
         // omp parallel for
-        for (int i = 0; i < args.length; i ++) {
-            if(isPrime(Long.parseLong (args[i])))
-                System.out.printf ("%s%n", args[i]);
+        for (int i = 0; i < args.length; i++) {
+            if (isPrime(Long.parseLong(args[i])))
+                System.out.printf("%s%n", args[i]);
         }
     }
 }
