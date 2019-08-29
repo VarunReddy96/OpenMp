c35dd840e2247ae3a57d969d4735097809ccc4b8
nikhil keswaney <nikhilkeswaney1996@gmail.com>
2019-08-28 20:31:07 -0400

    commented code

--- src/main/java/edu/rit/cs/pi/PiSeq.java
+++ src/main/java/edu/rit/cs/pi/PiSeq.java
@@ -1,18 +1,38 @@
 
 
 import java.util.Random;
+
+/**
+ * Class PiSeq is a sequential program that calculates an approximate value for
+ * &pi; using a Monte Carlo technique. The program generates a number of random
+ * points in the unit square (0,0) to (1,1) and counts how many of them lie
+ * within a circle of radius 1 centered at the origin. The fraction of the
+ * points within the circle is approximately &pi;/4.
+ **/
 public class PiSeq {
+
+    /**
+     * Main program.
+     */
     public static void main(String[] args) throws Exception {
+        Random prng;
+
         long seed;
         long N;
-        Random prng;
         long count;
+
+        // Validate command line arguments.
+        if (args.length != 2)
+            usage();
 
         seed = Long.parseLong(args[0]);
         N = Long.parseLong(args[1]);
 
+        // Set up PRNG.
         prng = new Random(seed);
 
+        // Generate n random points in the unit square, count how many are in
+        // the unit circle.
         count = 0;
         for (long i = 0; i < N; ++i) {
             double x = prng.nextDouble();
@@ -23,5 +43,12 @@ public class PiSeq {
 
         System.out.printf("pi = 4*%d/%d = %.9f%n", count, N, 4.0 * count / N);
     }
-
+    /**
+     * Print a usage message and exit.
+     */
+    private static void usage() {
+       System.err.println ("Usage: java PiSeq <seed> <N>");
+       System.err.println ("<seed> = Random seed");
+       System.err.println ("<N> = Number of random points");
+    }
 }
--- src/main/java/edu/rit/cs/pi/PiSmp.java
+++ src/main/java/edu/rit/cs/pi/PiSmp.java
@@ -1,23 +1,41 @@
 import java.util.Random;
 import java.util.Arrays;
 
+/**
+ * Class PiSmp is a parallel program that calculates an approximate value for
+ * &pi; using a Monte Carlo technique. The program generates a number of random
+ * points in the unit square (0,0) to (1,1) and counts how many of them lie
+ * within a circle of radius 1 centered at the origin. The fraction of the
+ * points within the circle is approximately &pi;/4.
+ **/
 public class PiSmp {
 
 
-
+    /**
+     * Main program.
+     */
     public static void main(String[] args) throws Exception {
         long seed;
         long N;
 
         long count;
 
+        // Validate command line arguments.
+        if (args.length != 2)
+            usage();
+
         seed = Long.parseLong(args[0]);
         N = Integer.parseInt(args[1]);
 
+
         long[] threadCounter = new long[Runtime.getRuntime().availableProcessors()];
+
+        // Generate n random points in the unit square, count how many are in
+        // the unit circle.
 
         // omp parallel
         {
+
             Random prng = new Random(seed);
             for (long i = 0; i < N / OMP4J_NUM_THREADS; ++i) {
 
@@ -43,4 +61,12 @@ public class PiSmp {
         System.out.print(4.0 * threadCounter[0] / N);
     }
 
+    /**
+     * Print a usage message and exit.
+     */
+    private static void usage() {
+        System.err.println("Usage: java PiSeq <seed> <N>");
+        System.err.println("<seed> = Random seed");
+        System.err.println("<N> = Number of random points");
+    }
 }
