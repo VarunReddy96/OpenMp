a31d1a67487beaa2fc77af2bea5b7666bffbb723
VarunReddy96 <varunreddy20596@gmail.com>
2019-09-10 16:53:07 -0400

    Updated code to be reviewed by the professor

--- lemoine_conjecture/src/main/java/edu/rit/cs/lemoineconjec.java
+++ lemoine_conjecture/src/main/java/edu/rit/cs/lemoineconjec.java
@@ -28,13 +28,21 @@ public class lemoineconjec {
     public int q = 0;
     public int counting = 0;
 
-    public lemoineconjec(){} //default constructor
+    public lemoineconjec() {
+    } //default constructor
+
+
+    public void checkarguments(int x, int y) throws Exception {
+        if (x % 2 == 0 || y % 2 == 0 || y <= x || x <= 5) {
+            throw new Exception();
+        }
+    }
+
 
     /**
      * This method creates a list of q values and verifies the lemoine conjecture for the num inputed.
      *
      * @param num: The num to verify lemoine conjecture.
-     *
      */
 
     public void findQ(int num) {
@@ -53,19 +61,18 @@ public class lemoineconjec {
     /**
      * This method is a helper method to verify the lemoine conjecture for the num inputed.
      *
-     * @param num: The num to verify lemoine conjecture.
+     * @param num:   The num to verify lemoine conjecture.
      * @param qlist: the list of suitable q values.
-     *
      */
 
     public void checktheorm(ArrayList<Integer> qlist, int num) {
-        for(int j = (qlist.size()-1);j>=0;j--){ //the list is reversed so as to find the value of p quickly
-            if((num-2*qlist.get(j))!=2 && Prime.isPrime((num-2*qlist.get(j)))){ //checking if the p value is not even and is a prime number.
-                if (this.p < (num-2*qlist.get(j))) {
-                    this.p = (num-2*qlist.get(j));
+        for (int j = (qlist.size() - 1); j >= 0; j--) { //the list is reversed so as to find the value of p quickly
+            if ((num - 2 * qlist.get(j)) != 2 && Prime.isPrime((num - 2 * qlist.get(j)))) { //checking if the p value is not even and is a prime number.
+                if (this.p < (num - 2 * qlist.get(j))) {
+                    this.p = (num - 2 * qlist.get(j));
                     this.q = qlist.get(j);
                     this.maxnum = num;
-                } else if (this.p == (num-2*qlist.get(j))) {
+                } else if (this.p == (num - 2 * qlist.get(j))) {
                     if (this.q < qlist.get(j)) {
                         this.q = qlist.get(j);
                         this.maxnum = num;
@@ -86,24 +93,25 @@ public class lemoineconjec {
 
     public static void main(String[] args) {
         // takes in 2 command line arguments
+        lemoineconjec t = new lemoineconjec();
         try {
-            int x = Integer.parseInt(args[0]);
-            lemoineconjec t = new lemoineconjec();
-            long starttime = System.currentTimeMillis();
-            while (x <= Integer.parseInt(args[1])) {
-                if (x % 2 != 0) {
-                    t.findQ(x);
-                }
-                x++;
-            }
-            long endtime = System.currentTimeMillis();
-            System.out.println("The value to be printed in main is " + t.maxnum + " = " + t.p + " + 2 * " + ((t.maxnum - t.p) / 2));
-            System.out.println("The operations are: " + t.counting);
-            System.out.println("Time elapsed : "+(endtime-starttime));
+            t.checkarguments(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
 
         } catch (Exception e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace();
+            System.out.println("Enter valid integers values");
+            return;
         }
+        int x = Integer.parseInt(args[0]);
+        long starttime = System.currentTimeMillis();
+        while (x <= Integer.parseInt(args[1])) {
+            if (x % 2 != 0) {
+                t.findQ(x);
+            }
+            x++;
+        }
+        long endtime = System.currentTimeMillis();
+        System.out.println("The value to be printed in main is " + t.maxnum + " = " + t.p + " + 2 * " + ((t.maxnum - t.p) / 2));
+        System.out.println("The operations are: " + t.counting);
+        System.out.println("Time elapsed : " + (endtime - starttime));
     }
 }
